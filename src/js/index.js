61e99e83d4ff20e15910125b5e35031744846b8f
suzuki noriko <suzuki_noriko@liginc.co.jp>
2017-07-20 00:25:49 +0900

    jquery.pjax追加

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,17 @@ $(function(){
     $.pjax({
         area : '.l-body',
         link : 'a:not([target])',
-        ajax: { timeout: 2000},
-        wait: 500
+        // ajax: { timeout: 2000},
+        // wait: 500
     });
 
-    $(document).bind('pjax:fetch', function(){
-        $('body').css('overflow', 'hidden');
-        $('.l-body').attr({'class': 'fadeOut'});
-    });
-    $(document).bind('pjax:render', function(){
-        $('.l-body').attr({'class': 'fadeIn'});
-        $('body').css('overflow', '');
-    });
+    // $(document).bind('pjax:fetch', function(){
+    //     $('body').css('overflow', 'hidden');
+    //     $('.l-body').attr({'class': 'fadeOut'});
+    // });
+    // $(document).bind('pjax:render', function(){
+    //     $('.l-body').attr({'class': 'fadeIn'});
+    //     $('body').css('overflow', '');
+    // });
 
 });
