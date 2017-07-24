33b959616af2a232456748bd508ef9a7f72867d3
Noriko Suzuki <norikosuzuki@norikosuzuki-iMac.local>
2017-07-24 20:19:00 +0900

    index.js調整

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,27 @@ $(function(){
         wait: 600
     });
 
-    $(document).bind('pjax:fetch', function(){
-        $('body').css('overflow', 'hidden');
-        $('.l-body').attr({'class': 'fadeOut l-body'});
-    });
-    $(document).bind('pjax:render', function(){
-        $('.l-body').attr({'class': 'fadeIn l-body'});
-        $('body').css('overflow', '');
+    // $(document).bind('pjax:fetch', function(){
+    //     $('body').css('overflow', 'hidden');
+    //     $('.l-body').attr({'class': 'bookOut l-body'});
+    // });
+    // $(document).bind('pjax:render', function(){
+    //     $('.l-body').attr({'class': 'bookIn l-body'});
+    //     $('body').css('overflow', '');
+    // });
+
+    $(document).on('pjax:fetch',function(){ // 消える時の処理
+        var windowScr = $(window).scrollTop();
+
+        if(windowScr){
+           $('body,html').animate({scrollTop : 0},'200');
+           $('.l-body').removeClass('bookIn').addClass('bookOut');
+        }else{
+           $('.l-body').removeClass('bookIn').addClass('bookOut');
+        }
     });
 
+    $(document).on('pjax:render',function(){ // 現れる時の処理
+        $('.l-body').removeClass('bookOut').addClass('bookIn');
+    });
 });
